170c11d2e7b6cb4496a1c63d9c80c62608b891c0
Игорь Сидоров <desper74@mail.ru>
2023-07-28 19:17:35 +0300

    Add corrected 1 file

---
+++ temp_1.py
@@ -0,0 +1,13 @@
+import math
+
+# Спросим, что хорошего в этой библиотеке.
+print(math.__doc__)
+
+# Будет напечатано:
+# This module provides access to the mathematical functions
+# defined by the C standard.
+
+# Этот модуль обеспечивает доступ к математическим функциям
+# определяется стандартом C.
+
+print(print.__doc__)
